6d9ea95b54c9078c45791f6224465e0ce86f2ba3
Simon Egebjerg Jensen <simo897d@edu.ucl.dk>
2019-09-06 11:06:59 +0200

    Login site and CreateUser site. Some CSS

---
+++ VitecMVPrivat/Models/UserInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecMVPrivat.Models
+{
+    public class UserInformation
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
