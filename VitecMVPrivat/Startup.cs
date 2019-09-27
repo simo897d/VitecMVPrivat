8ec4b1f81dccb0a6315c109c1d454050a3913f0d
Simon Egebjerg Jensen <simo897d@edu.ucl.dk>
2019-09-27 11:35:41 +0200

    Can launch in Development and Staging

--- VitecMVPrivat/Startup.cs
+++ VitecMVPrivat/Startup.cs
@@ -43,12 +43,13 @@ namespace VitecMVPrivat
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            if(env.IsProduction() || env.IsStaging())
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
